8a07328bdceff9beb797670df958e8d80036ae80
Sineth Shashintha <sinethshashintha2000@gmail.com>
2023-09-11 20:13:09 +0530

    components creation

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,13 +1,29 @@
 import "./TodoInput.css";
 import {useState} from 'react';
+import {PlusIcon} from '@heroicons/react/outline';
 
-function TodoInput(){
+function TodoInput({todos, setTodos}){
     const [todo, setTodo] = useState("");
+
+    const handleClick = () => {
+        const newTodo ={
+            id:1,
+            name:todo,
+            isCompleted:false,
+        }
+        const newTodos = [...todos, newTodo];
+        setTodos(newTodos);
+    }
+
     return(
         <div>
             <input type="text" value={todo} onChange={(event) => {
                 setTodo(event.target.value);
             }}/>
+
+            <button onClick={handleClick}>
+                <PlusIcon  style={{width: "18px", height:"18px"}}/>
+            </button>
         </div>
     )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 function TodoList(){
+
+    const [todos, setTodos] = useState([]);
+
     return(
         <div>
             <h2>My Todos</h2>
-            <TodoInput />
+            <TodoInput todos={todos} setTodos={setTodos}/>
             <div>
-                <TodoItem />
+                {todos.map((item) => {
+                    return <TodoItem key={item.id}/>
+                })}
             </div>
         </div>
     )
